209a4c830d28b9b8129873bcb34632f2559a5070
Hich <contact@hitch.fr>
2022-01-02 15:39:01 +0100

    create is_file utility function

--- utils.sh
+++ utils.sh
@@ -1,7 +1,17 @@
+# Test if the given path ${1} is
+# a real file on the file system
+function is_file() {
+  if [[ -f "${1}" ]]
+  then
+    return 0;
+  else
+    return 1;
+  fi
+}
+
 # Separate each part of the given string ${2} by the given
 # delimiter ${1} and return them in an array
-# from https://memoire-grise-liberee.fr.eu.org/Bash/function/strings/explode/
-
+# from https://memoire-grise-liberee.fr.eu.org/Bash/function/strings/explode
 function explode() {
     local delimiter="$1" string="$2"
     local IFS="${delimiter}"; shift; read -a array <<< "${string}";
