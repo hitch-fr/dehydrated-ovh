8b8ea24d115204a3dd6ca1e6bde4924bbe1f3481
Hich <contact@hitch.fr>
2022-01-02 10:20:47 +0100

    bash failsafe

--- hook.sh
+++ hook.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+# dehydrated-ovh by hitch.fr
+# website: https://hitch.fr
+#
+# This script is licensed under The GNU AFFERO GENERAL PUBLIC LICENSE.
+
+# Fail safe
+set -f # disable globbing
+set -e # exit on script fail
+set -E # ERR trap not fire in certain scenarios with -e only
+set -u # exit on var error
+set -x # print commands before exec (debug)
+set -o pipefail
